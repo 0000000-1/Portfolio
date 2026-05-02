6cabcb211d420c0cccfe41055b9e8d1a828c2cf4
sumit <rajaji9968@gmail.com>
2026-05-02 23:12:57 +0530

    changes to vite config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // This was missing
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: "/Portfolio/", 
   plugins: [
-    tailwindcss(),
+    react(),
+    tailwindcss()
   ],
-   base: "/Portfolio/", // This must match your repo name exactly
-  plugins: [react()],
 })
